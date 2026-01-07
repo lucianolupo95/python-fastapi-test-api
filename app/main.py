40dab37db243f461ff7734ff3274207543eb9aed
LucianoLupo95 <lnlupo95@gmail.com>
2026-01-07 14:28:27 +0100

    Test deploy

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI(title="python-fastapi-test-api")
 
+
 @app.get("/health")
 def health():
-    return {"status": "ok", "service": "python-fastapi-test-api"}
+    return {"status": "ok", "service": "python-fastapi-test-api running"}
